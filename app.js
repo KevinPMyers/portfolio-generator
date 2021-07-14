eae5aff1e3fa518a56397141358d5418ec7b8002
KevinPMyers <kmyers25@gmail.com>
2021-07-13 22:01:08 -0400

    took generatepage function and put it in its own src folder/file. Linked it to main app page with require

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+
+const [userName, github] = profileDataArgs;
 
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0 ; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log('================');
+fs.writeFile('./index.html', generatePage(userName, github), err => {
+    if (err) throw new Error(err);
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     // This for loop is the same as...
+//     for (let i = 0 ; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+// // Using the foreach() method
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
     
-};
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
