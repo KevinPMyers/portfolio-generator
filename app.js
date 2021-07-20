f7f5b12dfdd09f419bea92d74d980873868fb673
KevinPMyers <kmyers25@gmail.com>
2021-07-19 22:26:32 -0400

    Final product

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require("./src/page-template.js");
 const inquirer = require("inquirer");
 
@@ -138,25 +138,22 @@ const promptProject = (portfolioData) => {
 
 promptUser()
   .then(promptProject)
-  .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile("./dist/index.html", pageHTML, (err) => {
-      if (err) {
-          console.log(err);
-          return;
-      }
-      console.log('Page Created! Check out index.html in this directory to see it!');
-
-      fs.copyFile('./src/style.css', './dist/style.css', err => {
-          if (err) {
-              console.log(err);
-              return;
-          }
-          console.log('Stlye Sheet copied succesfully!');
-      });
-    });
-});
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 // const printProfileData = profileDataArr => {
 //     // This for loop is the same as...
 //     for (let i = 0 ; i < profileDataArr.length; i++) {
